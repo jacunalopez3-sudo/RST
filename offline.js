5b707c7f9b122eb8ad0a52b63ac2801d89fcdaae
jacunalopez3-sudo <jacunalopez3@gmail.com>
2026-07-15 22:48:38 -0600

    Restaurar visor PDF con nombre en URL firmada

--- offline.js
+++ offline.js
@@ -1,7 +1,7 @@
 (() => {
   if (!("serviceWorker" in navigator)) return;
 
-  const VERSION = window.BENNU_APP_VERSION || "2026.07.15.1";
+  const VERSION = window.BENNU_APP_VERSION || "2026.07.15.2";
   const RELOAD_KEY = "bennu-sw-reloaded-version";
   let reloading = false;
 
